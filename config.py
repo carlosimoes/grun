ffe4fb310e5593a9a6a6586eb828449052c5cd4d
carlosimoes <carlos96simoes@gmail.com>
2025-07-16 10:10:23 +0100

    change neo4j cred

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Settings(BaseModel):
     agent_model: str = "gemini-2.0-flash"
     fda_api_url: str = "https://api.fda.gov/drug/event.json"
     logging_level: int = logging.INFO
-    neo4j_uri: str = "bolt://44.200.67.104"
+    neo4j_uri: str = "bolt://35.171.8.98"
     neo4j_username: str = "neo4j"
-    neo4j_password: str = "combustion-majorities-pine"
+    neo4j_password: str = "amount-oar-cardboard"
     google_api: str = "AIzaSyDHaughVboCMlu1m3TzIhHpH_5nkav5SN4"
     vertexai_use: str = "FALSE"
 
